123afcb777685de49b244f22bef38f78a3ea7635
Nikolay Vasilev <nikolay.vasilev@sap.com>
2021-02-08 10:07:33 +0200

    WIP
    Co-authored-by: Nikolay Vasilev <nikolay.vasilev@sap.com>

--- src/index.php
+++ src/index.php
@@ -1,41 +1,49 @@
 <html>
+
 <head>
     <title>PHP-Test</title>
 </head>
+
 <body>
-<form method="post">
+    <form method="post">
+        Server Name: <label for="servername">
+            <input type="text" servername="servername">
+            <br><br>
+        PHP Version: <label for="version">
+                <select name="version">
+                    <option value="5.6">5.6</option>
+                    <option value="7">7</option>
+                    <option value="7.3">7.3</option>
+                    <option value="7.4">7.4</option>
+                    <option value="8">8</option>
+                </select>
+            </label>
+            <br><br>
+            <input type="submit" name="submit" value="Submit">
+    </form>
+    <?php
+    function create_file($filename, $content)
+    {
+        $myfile = fopen($filename . ".Dockerfile", "w") or die("Unable to open file!");
 
-    <label for="version"> PHP Version
-        <select name="version">
-            <option value="8">8</option>
-            <option value="7.4">7.4</option>
-            <option value="7.3">7.3</option>
-            <option value="7">7</option>
-            <option value="5.6">5.6</option>
-        </select>
-    </label>
-    <input type="submit" name="submit" value="Submit">
-</form>
-<?php
-function create_file($filename, $content)
-{
-    $myfile = fopen($filename . ".Dockerfile", "w") or die("Unable to open file!");
+        fwrite($myfile, $content);
+        fclose($myfile);
+    }
+    function generateDockerfileContent($version)
+    {
+        $content = "FROM php:" . $version . "-apache\n";
+        $content .= "COPY . /var/www/html/";
 
-    fwrite($myfile, $content);
-    fclose($myfile);
-}
-function generateDockerfileContent($version) {
-    $content = "FROM php:" . $version . "-apache\n";
-    $content .= "COPY . /var/www/html/";
+        return $content;
+    }
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $version = $_POST["version"];
+        $servername = $_POST["servername"];
+        $dockerfileContent = generateDockerfileContent($version);
+        create_file($servername, $dockerfileContent);
+    }
 
-    return $content;
-}
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $version = $_POST["version"];
-    $dockerfileContent = generateDockerfileContent($version);
-    create_file("gosho", $dockerfileContent);
-}
-
-?>
+    ?>
 </body>
+
 </html>
